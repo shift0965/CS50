941556ddf2b090ff233e2d4aa3a3a636e012584a
shift0965 <shift0965@gmail.com>
2022-05-16 19:37:12 +0800

    Practice Object Oriented

--- hello.py
+++ hello.py
@@ -1,3 +1,7 @@
+from decimal import InvalidContext
+from multiprocessing.sharedctypes import Value
+
+
 print("hello world!")
 #name = input("What is you name: ")
 #print("hello {}".format(name))
@@ -8,3 +12,16 @@ def square(x):
 
 for i in range(10):
     print("The square of {} is {}".format(i, square(i)))
+
+try:
+    x = int(input("Input a number: "))
+    y = int(input("Another number: "))
+except ValueError:
+    print("ValueError")
+    exit(1)
+
+try:
+    print("x/y = {}".format(x/y))
+except ZeroDivisionError:
+    print("ZeroDivisionError")
+    exit(1)
