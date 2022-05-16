8fa2006cfe02710d97ee494a654487283368b401
shift0965 <shift0965@gmail.com>
2022-05-16 16:58:46 +0800

    create simple python file

---
+++ hello.py
@@ -0,0 +1,10 @@
+print("hello world!")
+#name = input("What is you name: ")
+#print("hello {}".format(name))
+
+
+def square(x):
+    return x*x
+
+for i in range(10):
+    print("The square of {} is {}".format(i, square(i)))
